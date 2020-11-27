f81e0c93820ec798debadc04e414a32a7a5fe530
IMANEBOUSSHINEE <67756609+IMANEBOUSSHINEE@users.noreply.github.com>
2020-11-27 01:16:39 +0100

    search.c
    search function for hashtable.c

---
+++ search.c
@@ -0,0 +1,19 @@
+struct DataItem *search(int key) {
+   //get the hash 
+   int hashIndex = hashCode(key);  
+	
+   //move in array until an empty 
+   while(hashArray[hashIndex] != NULL) {
+	
+      if(hashArray[hashIndex]->key == key)
+         return hashArray[hashIndex]; 
+			
+      //go to next cell
+      ++hashIndex;
+		
+      //wrap around the table
+      hashIndex %= SIZE;
+   }        
+	
+   return NULL;        
+}
